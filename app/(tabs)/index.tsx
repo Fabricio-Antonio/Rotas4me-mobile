694c1976baed2929bdace45833defe12fdf42664
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2025-06-20 07:06:42 -0300

    :sparkles: feat: add basic map and function to get currtent position

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,87 @@
+import { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
-
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  watchPositionAsync,
+  LocationAccuracy,
+  LocationObject,
+} from 'expo-location';
 import { Text, View } from '@/components/Themed';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function TabOneScreen() {
+  const [location, setLocation] = useState<LocationObject | null>(null);
+
+  useEffect(() => {
+    getLocationAsync();
+  }, []);
+
+  useEffect(() => {
+    let subscription: any;
+
+    const startWatching = async () => {
+      subscription = await watchPositionAsync(
+        {
+          accuracy: LocationAccuracy.High,
+          timeInterval: 1000,
+          distanceInterval: 1,
+        },
+        (newLocation) => {
+          setLocation(newLocation);
+        }
+      );
+    };
+
+    startWatching();
+
+    return () => {
+      if (subscription) {
+        subscription.remove();
+      }
+    };
+  }, []);
+
+  async function getLocationAsync() {
+    const { granted } = await requestForegroundPermissionsAsync();
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+      console.log('Current Position:', currentPosition);
+    } else {
+      console.log('Permissão de localização negada.');
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>HOME PAGE</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>Olá, usuária!</Text>
+      {location && (
+        <>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005,
+            }}
+          >
+            <Marker
+              coordinate={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+              }}
+              title="Você está aqui"
+            />
+          </MapView>
+          <Text>
+            Latitude: {location.coords.latitude}
+            {'\n'}
+            Longitude: {location.coords.longitude}
+          </Text>
+        </>
+      )}
     </View>
   );
 }
@@ -21,9 +96,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  map: {
+    width: '78%',
+    height: '40%',
+    marginVertical: 10,
   },
 });
