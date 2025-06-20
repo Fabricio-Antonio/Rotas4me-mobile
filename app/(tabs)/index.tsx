4238665effb5fb5c525283195a1b85450682730b
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2025-06-20 18:18:37 -0300

    :lipstick: feat: conclud home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import {
   requestForegroundPermissionsAsync,
   getCurrentPositionAsync,
@@ -10,12 +10,15 @@ import {
 import { Text, View } from "@/components/Themed";
 import MapView, { Marker } from "react-native-maps";
 import { useFonts } from "expo-font";
+import { Image } from "react-native";
+import { View as RNView } from "react-native";
 
 export default function TabOneScreen() {
   const [location, setLocation] = useState<LocationObject | null>(null);
 
   const [fontsLoaded] = useFonts({
     "Poppins-SemiBold": require("../../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Medium": require("../../assets/fonts/Poppins-Medium.ttf"),
   });
 
   useEffect(() => {
@@ -61,10 +64,9 @@ export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.title}>Olá, usuária!</Text>
+        <Text style={styles.title}>Olá, usuária! </Text>
       </View>
       {location && (
-        <>
           <MapView
             style={styles.map}
             initialRegion={{
@@ -82,19 +84,18 @@ export default function TabOneScreen() {
               title="Você está aqui"
             />
           </MapView>
-        </>
       )}
       <View style={styles.recomendation}>
-        <Text style={styles.recomendationText}>
-          Ações recomendadas
-        </Text>
+        <Text style={styles.recomendationText}>Ações recomendadas</Text>
       </View>
       <View style={styles.cardsContainer}>
-        
-        <View
-          style={[styles.recomendationCard, { justifyContent: "flex-start" }]}
-        >
-          <Text style={styles.cardText}>Iniciar nova rota</Text>
+        <View style={[styles.recomendationCard, styles.cardNewRoute]}>
+          <Text style={styles.cardText}>Iniciar{"\n"}nova rota</Text>
+          <Image
+            source={require("../../assets/images/hand.png")}
+            style={{ width: 60, height: 60 }}
+            resizeMode="contain"
+          />
         </View>
         <View style={[styles.recomendationCard, { justifyContent: "center" }]}>
           <Text style={styles.cardText}>
@@ -103,15 +104,23 @@ export default function TabOneScreen() {
         </View>
         <View style={[styles.recomendationCard, { justifyContent: "center" }]}>
           <Text style={styles.cardText}>
-            Iniciar preenchimento de um Boletim de Ocorrência
+            Iniciar preenchimento de um{" "}
+            <Text style={styles.boldText}>Boletim de Ocorrência</Text>
           </Text>
         </View>
-        <View
-          style={[styles.recomendationCard, { justifyContent: "flex-end" }]}
-        >
-          <Text style={styles.cardText}>
-            Localizar posto da Polícia Federal mais próximo
-          </Text>
+        <View style={[styles.recomendationCard, styles.cardLocateStation]}>
+          <Image
+            source={require("../../assets/images/lupe.png")}
+            style={styles.iconStyle}
+            resizeMode="contain"
+          />
+          <RNView style={styles.cardLocateTextWrapper}>
+            <Text style={styles.cardLocateText}>
+              Localizar{"\n"}posto da{"\n"}
+              <Text style={styles.boldText}>PF</Text>
+              {"\n"}próximo
+            </Text>
+          </RNView>
         </View>
       </View>
     </View>
@@ -127,6 +136,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 38,
     fontFamily: "Poppins-SemiBold",
+    marginTop: 20,
   },
   map: {
     width: "78%",
@@ -158,9 +168,10 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   cardText: {
-    color: "#fff",
-    fontFamily: "Poppins-SemiBold",
+    fontFamily: "Poppins-Medium",
     paddingHorizontal: 10,
+    fontSize: 12,
+    color: "#FFFFFF",
   },
   recomendationCard: {
     width: "48%",
@@ -171,5 +182,44 @@ const styles = StyleSheet.create({
     padding: 10,
     display: "flex",
     alignItems: "flex-start",
+  },
+  cardNewRoute: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  cardLocateStation: {
+    backgroundColor: "#D65E75",
+    flexDirection: "row",
+    alignItems: "center",
+    height: 110,
+    width: "48%",
+    borderRadius: 12,
+    textAlign: "left",
+  },
+  iconStyle: {
+    width: 40,
+    height: 40,
+    marginRight: 8,
+  },
+
+  cardLocateTextWrapper: {
+    flex: 1,
+    flexWrap: "wrap",
+    justifyContent: "center",
+    maxWidth: "70%",
+  },
+
+  cardLocateText: {
+    fontFamily: "Poppins-Medium",
+    fontSize: 12,
+    color: "#FFFFFF",
+    textAlign: "left",
+    lineHeight: 16,
+    flexShrink: 1,
+  },
+
+  boldText: {
+    fontFamily: "Poppins-SemiBold",
+    color: "#FFFFFF",
   },
 });
